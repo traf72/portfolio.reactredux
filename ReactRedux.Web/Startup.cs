7a7b916a9e4266e45a41b8e1690ff0ac191616af
traf72 <traf72@gmail.com>
2019-03-05 22:48:56 +0300

    Run webpack-dev-server in Startup.cs

--- ReactRedux.Web/Startup.cs
+++ ReactRedux.Web/Startup.cs
@@ -89,8 +89,8 @@ namespace ReactRedux.Web
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
-                    //spa.UseReactDevelopmentServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+                    spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
         }
